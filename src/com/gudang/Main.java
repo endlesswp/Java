282dcacb5161578f5e12062217964e35bbd52879
Endlesswp <endleswpangrib@gmail.com>
2025-10-06 07:09:29 +0700

    all

--- src/com/gudang/Main.java
+++ src/com/gudang/Main.java
@@ -25,4 +25,10 @@ public class App extends Application{
     public static void main(String[] args) {
     launch(args);
     }
+
+    private static class FXMLLoader {
+
+        public FXMLLoader() {
+        }
+    }
 }
